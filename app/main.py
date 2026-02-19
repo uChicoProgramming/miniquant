7663568bc324ede626ae5879fb335773ab52b1a8
uChicoPrograming <franciscobusiness3007@gmail.com>
2026-02-19 15:26:05 -0300

    Configuração do ambiente

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 def read_root():
-    return {'Hello': 'World'}
+    return {"✨✨ menssagem": "Servidor projeto quant rodando! ✨✨"}
---
+++ tests/app/test_main.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+
+def test_root():
+    client = TestClient(app)
+
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "✨✨ menssagem":"Servidor projeto quant rodando! ✨✨"
+        }
