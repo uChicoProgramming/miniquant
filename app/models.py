cd33207b7e45ad43b135fc88252b28a75903abc7
Francisco Ferreira <franciscobusiness3007@gmail.com>
2026-02-20 04:06:25 -0300

    Atualizando poetry.lock

--- app/models.py
+++ app/models.py
@@ -1,21 +1,16 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import (
     Mapped,
-    mapped_as_dataclass,
-    mapped_as_dataclass,
     mapped_column,
     registry,
-    relationship,
 )
 
 table_registry = registry()
 
 
-class Base(declarative_base):
-    ...
+class Base(declarative_base): ...
 
 
 class Assets(Base):
-    __tablename__ = 'assets'
+    __tablename__ = "assets"
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
-    
